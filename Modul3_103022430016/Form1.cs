e8d35c19362a6f1435bacafba3692db1f70350e4
Rizkya Ramdan <rizkyaramdancringe@gmail.com>
2026-03-14 08:35:12 +0700

    " first commit & bikin GUI (ini ketinggalan) "

---
+++ Modul3_103022430016/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Modul3_103022430016
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            string[] comboBox_Option = { "Celcius", "Fahrenheit" , "Kelvin", "Reamur"};
+
+            comboBox1.DataSource = comboBox_Option;
+            comboBox2.DataSource = comboBox_Option.Clone();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
