a9e63a7f012c7c077e5f7d6d7c6a8e146a904662
Rizkya Ramdan <rizkyaramdancringe@gmail.com>
2026-03-14 09:17:32 +0700

    " NAMBAHIN IMPLEMENTASI KODE PROGRAM"

---
+++ Modul3_103022430016/Modul3_103022430016/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Modul3_103022430016
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            string[] comboBox_Option = { "Celcius", "Fahrenheit" , "Kelvin", "Reamur"};
+
+            comboBox1.DataSource = comboBox_Option;
+            comboBox2.DataSource = comboBox_Option.Clone();
+        }
+
+        private double ConvertTempature(double value, string fromUnit, string toUnit)
+        {
+            double result = value;
+            if (fromUnit == "Fahrenheit")
+                result = (value - 32) * 5 / 9;
+            else if (fromUnit == "Kelvin")
+                result = value - 273.15;
+            else if (fromUnit == "Reamur")
+                result = value * 5 / 4;
+            if (toUnit == "Fahrenheit")
+                result = (result * 9 / 5) + 32;
+            else if (toUnit == "Kelvin")
+                result = result + 273.15;
+            else if (toUnit == "Reamur")
+                result = result * 4 / 5;
+            return result;
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Masukkan angka yang valid!");
+                return;
+            }
+            double value = Convert.ToDouble(textBox1.Text);
+                
+            String fromUnit = comboBox1.SelectedItem.ToString();
+            String toUnit = comboBox2.SelectedItem.ToString();
+
+            double result = ConvertTempature(value, fromUnit, toUnit);
+
+            textBox2.Text = result.ToString("F2");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Modul3_103022430016/Modul3_103022430016/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Modul3_103022430016")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Modul3_103022430016")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("16f10c22-2b5b-4800-8808-667aad2276b1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
